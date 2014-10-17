d9e757c211d4365f56f16fd243f258e317b85e4f
Gary Johnson <gary@gjtt.com>
2014-10-17 09:35:42 -0400

    Adding support for Qt5.2

--- pqaut/automator/factory.py
+++ pqaut/automator/factory.py
@@ -9,17 +9,17 @@ def automate(target):
     automators = ['QQuickWindow', 'QQuickItem', 'QQuickWidget', 'QWidget', 'QObject']
 
     for class_name in automators:
-        qt_type = getattr(PyQt5.Qt, class_name)
         try:
+            qt_type = getattr(PyQt5.Qt, class_name)
             automator_module = importlib.import_module('pqaut.automator.{}'.format(class_name.lower()))
+
+            automator_type = getattr(automator_module, '{}Automator'.format(class_name))
+
+            if qt_type is not None and automator_type is not None and isinstance(target, qt_type):
+                return automator_type(target)
         except:
             logger.debug('could not import {}'.format(class_name))
             continue
-
-        automator_type = getattr(automator_module, '{}Automator'.format(class_name))
-
-        if qt_type is not None and automator_type is not None and isinstance(target, qt_type):
-            return automator_type(target)
 
     raise Exception('No automator found for {}'.format(target))
 
