fd8cb76e86c7c2287d3b19010cc025facc10363d
Gary Johnson <gary@gjtt.com>
2014-11-13 23:11:12 -0500

    v0.3.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='pqaut',
-    version='0.3.5',
+    version='0.3.6',
     author='Gary Johnson',
     author_email = 'gary@gjtt.com',
     description = 'Automation for BDD testing PyQt apps',
